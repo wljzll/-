e47f13a1a08e477c04b9aa417d736130356044f2
1826865187@qq.com <1826865187@qq.com>
2021-06-24 16:59:30 +0800

    修改

--- 6.防抖节流.js
+++ 6.防抖节流.js
@@ -1,41 +1,4 @@
 /**
- * @desc 函数节流
- * @param func 函数
- * @param wait 延迟执行毫秒数
- * @param type 1 表时间戳版（前置触发），2 表定时器版（后置触发）
- */
-function throttle(func, wait, type) {
-  if (type == 1) {
-    var previouse = 0;
-  } else {
-    var timeout;
-  }
-
-  return function () {
-    let context = this;
-    let args = arguments;
-    if (type == 1) {
-      let now = new Date();
-      if (now - previouse > wait) {
-        func.call(context, ...args);
-        previouse = now;
-      }
-    } else {
-      if (!timeout) {
-        timeout = setTimeout(() => {
-          timeout = null;
-          func.call(context, ...args);
-        }, wait);
-      }
-    }
-  };
-}
-function test() {
-  console.log("测试");
-}
-window.addEventListener("click", throttle(test, 2000, 2));
-
-/**
  * @desc 函数防抖
  * @param func 函数
  * @param wait 延迟执行毫秒数
@@ -71,3 +34,40 @@ function test() {
   console.log("测试");
 }
 window.addEventListener("click", debounce(test, 2000, true));
+
+/**
+ * @desc 函数节流
+ * @param func 函数
+ * @param wait 延迟执行毫秒数
+ * @param type 1 表时间戳版（前置触发），2 表定时器版（后置触发）
+ */
+function throttle(func, wait, type) {
+  if (type == 1) {
+    var previouse = 0;
+  } else {
+    var timeout;
+  }
+
+  return function () {
+    let context = this;
+    let args = arguments;
+    if (type == 1) {
+      let now = new Date();
+      if (now - previouse > wait) {
+        func.call(context, ...args);
+        previouse = now;
+      }
+    } else {
+      if (!timeout) {
+        timeout = setTimeout(() => {
+          timeout = null;
+          func.call(context, ...args);
+        }, wait);
+      }
+    }
+  };
+}
+function test() {
+  console.log("测试");
+}
+window.addEventListener("click", throttle(test, 2000, 2));
